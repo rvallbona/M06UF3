eeaf80e336f6b9caf3735557f826bd970eef6379
Ramon Vallbona <98883990+rvallbona@users.noreply.github.com>
2022-11-11 19:43:28 +0100

    Screen Ingredients

--- Assets/Scripts/DBManager.cs
+++ Assets/Scripts/DBManager.cs
@@ -1,33 +1,37 @@
 using Mono.Data.Sqlite;
 using System.Data;
 using UnityEngine;
-
+using System.Collections.Generic;
 public class DBManager : MonoBehaviour
 {
+    //DB
     IDbConnection dbConnection;
-
+    //Ingredients
+    public List<string> name_ingredients = new List<string>();
     private void Start()
     {
         OpenDatabase();
-
-        string query = "SELECT * FROM potion_types";
-
-        IDbCommand cmd = dbConnection.CreateCommand();
-        cmd.CommandText = query;
-
-        IDataReader dataReader = cmd.ExecuteReader();
-
-        while (dataReader.Read())
-        {
-            string potion_type = dataReader.GetString(1);
-            Debug.Log(potion_type);
-        }
+        Ingredients();
     }
-
     private void OpenDatabase()
     {
         string dbUri = "URI=file:alchENTImist.db";
         dbConnection = new SqliteConnection(dbUri);
         dbConnection.Open();
+    }
+    void Ingredients()
+    {
+        string ingredientsQuery = "SELECT* FROM ingredients";
+
+        IDbCommand cmd = dbConnection.CreateCommand();
+        cmd.CommandText = ingredientsQuery;
+
+        IDataReader dataReader = cmd.ExecuteReader();
+
+        while (dataReader.Read())
+        {
+            name_ingredients.Add(dataReader.GetString(1));
+            Debug.Log(name_ingredients);
+        }
     }
 }
