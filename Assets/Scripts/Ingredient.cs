7ab2afad6e1f67fdf5b1f2ccb441b7ca7ea46d35
rvallbona <joan.vallbona@enti.cat>
2022-11-19 11:38:37 +0100

    Try Ingredients List

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class ingredients : MonoBehaviour
+public class Ingredient : MonoBehaviour
 {
     public GameObject ingredient;
     DBManager dbManager;
     List<string> nameIngredientsList;
     List<int> idIngredientsList;
+    int id_ingredient;
+    string name_ingredient;
+    List<Ingredient> ingredients;
     void Start()
     {
         dbManager = GameObject.FindGameObjectWithTag("dbManager").GetComponent<DBManager>();
@@ -20,5 +23,10 @@ public class ingredients : MonoBehaviour
             TMPro.TextMeshProUGUI ingredientName = newIngredient.GetComponent<TMPro.TextMeshProUGUI>();
             ingredientName.text = idIngredientsList[i] + nameIngredientsList[i];
         }
+    }
+    public void SetIngredient(int id, string name)
+    {
+        id_ingredient = id;
+        name_ingredient = name;
     }
 }
